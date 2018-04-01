6e1d60eed9f20cddd4cf4dcb1485a5d2e9e752bd
Filipe Gonçalves <filipe@impossible.com>
2018-04-02 00:32:28 +0100

    working version using All In One container

---
+++ contracts/echidna.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.16;
+
+contract Test {
+  bool private flag0=true;
+  bool private flag1=true;
+
+  function set0(int val) private returns (bool){
+    if (val % 10 == 0) {flag0 = false;}
+  }
+  function set1(int val) private returns (bool){
+    if (val % 10 == 0 && flag0) {flag1 = false;}
+  }
+  function echidna_alwaystrue() public pure returns (bool){
+    return(true);
+  }
+  function echidna_sometimesfalse() public view returns (bool){
+    return(flag0 || flag1);
+  }
+}
