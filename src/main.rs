4b3dce32348ed0e7142ccfb1e5cf5280abd0bfd5
Filipe Gonçalves <filipe@impossible.com>
2018-06-14 00:19:53 +0100

    Add `json` and `silent`(ish) output formats

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 extern crate serde_derive;
 
 extern crate serde;
+
+#[macro_use]
 extern crate serde_json;
 
 #[macro_use]
@@ -51,6 +53,13 @@ fn main() {
                 .required(true),
         )
         .arg(
+            Arg::with_name("output-format")
+                .short("x")
+                .takes_value(true)
+                .possible_values(&["html", "json", "silent"])
+                .default_value("html"),
+        )
+        .arg(
             Arg::with_name("output")
                 .short("o")
                 .takes_value(true)
@@ -64,6 +73,7 @@ fn main() {
         .to_owned();
 
     let output_path = matches.value_of("output").unwrap();
+    let output_format = matches.value_of("output-format").unwrap();
 
     // very fast to complete, the penalty to run in parallel is unnecesary
     let solc_out = tools::run_solc(&contract_path);
@@ -82,50 +92,128 @@ fn main() {
     let myth_out = myth_handle.join().expect("Failed to run mythril");
     let oyente_out = oyente_handle.join().expect("Failed to run oyente");
 
-    let mut ctx = Context::new();
-    ctx.add("contract_file", cp_arc.as_ref());
-    match solc_out {
-        Some(s) => {
-            match s {
-                tools::SolcResponse::Success(j) => ctx.add("solc_out", &j),
-                tools::SolcResponse::Failure(s) => ctx.add("solc_err", &s),
-            }
-        }
-        _ => (),
-    }
-    match solium_out {
-        Some(s) => {
-            match s {
-                tools::SoliumResponse::Success(j) => ctx.add("solium_out", &j),
-                tools::SoliumResponse::Failure(s) => ctx.add("solium_err", &s),
-            }
-        }
-        _ => (),
-    }
-    match myth_out {
-        Some(s) => {
-            match s {
-                tools::MythrilResponse::Success(j) => ctx.add("myth_out", &j),
-                tools::MythrilResponse::Failure(s) => ctx.add("myth_err", &s),
-            }
-        }
-        _ => (),
-    }
-    match oyente_out {
-        Some(s) => {
-            match s {
-                tools::OyenteResponse::Success(j, b) => {
-                    ctx.add("oyente_out", &j);
-                    ctx.add("oyente_issues", &b)
+    match output_format {
+        "html" => {
+            let mut ctx = Context::new();
+            ctx.add("contract_file", cp_arc.as_ref());
+            match solc_out {
+                Some(s) => {
+                    match s {
+                        tools::SolcResponse::Success(j) => ctx.add("solc_out", &j),
+                        tools::SolcResponse::Failure(s) => ctx.add("solc_err", &s),
+                    }
                 }
-                tools::OyenteResponse::Failure(s) => ctx.add("oyente_err", &s),
+                _ => (),
+            }
+            match solium_out {
+                Some(s) => {
+                    match s {
+                        tools::SoliumResponse::Success(j) => ctx.add("solium_out", &j),
+                        tools::SoliumResponse::Failure(s) => ctx.add("solium_err", &s),
+                    }
+                }
+                _ => (),
+            }
+            match myth_out {
+                Some(s) => {
+                    match s {
+                        tools::MythrilResponse::Success(j) => ctx.add("myth_out", &j),
+                        tools::MythrilResponse::Failure(s) => ctx.add("myth_err", &s),
+                    }
+                }
+                _ => (),
+            }
+            match oyente_out {
+                Some(s) => {
+                    match s {
+                        tools::OyenteResponse::Success(j, b) => {
+                            ctx.add("oyente_out", &j);
+                            ctx.add("oyente_issues", &b)
+                        }
+                        tools::OyenteResponse::Failure(s) => ctx.add("oyente_err", &s),
+                    }
+                }
+                _ => (),
+            }
+
+            let idx = TERA.render("index.html", &ctx).expect(
+                "Failed to render reports",
+            );
+            fs::write(&output_path, &idx).expect("Unable to write file");
+        }
+        "json" => {
+            let all_encompassing_json_monstruosity = json!({
+                "solc" : match solc_out {
+                    Some(tools::SolcResponse::Success(s)) =>
+                    json!({"error": false, "result": s}),
+                    Some(tools::SolcResponse::Failure(s)) =>
+                     json!({"error": true, "result": s}),
+                    None => json!({"error": false, "result": ""}),
+                },
+                "solium" : match solium_out {
+                    Some(tools::SoliumResponse::Success(s)) =>
+                    json!({"error": false, "result": s}),
+                    Some(tools::SoliumResponse::Failure(s)) =>
+                    json!({"error": true, "result": s}),
+                    None => json!({"error": false, "result": ""}),
+                },
+                "mythril" : match myth_out {
+                    Some(tools::MythrilResponse::Success(s)) =>
+                    json!({"error": false, "result": s}),
+                    Some(tools::MythrilResponse::Failure(s)) =>
+                     json!({"error": true, "result": s}),
+                    None => json!({"error": false, "result": ""}),
+                },
+                "oyente" : match oyente_out {
+                    Some(tools::OyenteResponse::Success(s, _)) =>
+                    json!({"error": false, "result": s}),
+                    Some(tools::OyenteResponse::Failure(s)) =>
+                    json!({"error": true, "result": s}),
+                    None => json!({"error": false, "result": ""}),
+                }
+            });
+            let s = serde_json::to_string_pretty(&all_encompassing_json_monstruosity);
+            println!("{}", s.unwrap());
+        }
+        "silent" => {
+            let mut tools_with_issues = Vec::new();
+            match solc_out {
+                Some(tools::SolcResponse::Failure(_)) => tools_with_issues.push("solc"),
+                None => tools_with_issues.push("solc"),
+                _ => {}
+            };
+            match solium_out {
+                Some(tools::SoliumResponse::Success(ref l)) if l.len() > 0 => {
+                    tools_with_issues.push("solium")
+                }
+                Some(tools::SoliumResponse::Failure(_)) => tools_with_issues.push("solium"),
+                None => tools_with_issues.push("solium"),
+                _ => {}
+            };
+            match myth_out {
+                Some(tools::MythrilResponse::Success(ref o)) if !o.success => {
+                    tools_with_issues.push("solium")
+                }
+                Some(tools::MythrilResponse::Failure(_)) => tools_with_issues.push("mythril"),
+                None => tools_with_issues.push("mythril"),
+                _ => {}
+            };
+            match oyente_out {
+                Some(tools::OyenteResponse::Success(_, true)) => tools_with_issues.push("oyente"),
+                Some(tools::OyenteResponse::Failure(_)) => tools_with_issues.push("oyente"),
+                None => tools_with_issues.push("oyente"),
+                _ => {}
+            };
+            if tools_with_issues.len() == 0 {
+                println!("No issues found");
+            } else {
+                println!("Issues found in {}", tools_with_issues.join(", "));
             }
         }
-        _ => (),
+        _ => {
+            // should never happen
+            panic!("output format outside of allowed values");
+        }
     }
 
-    let idx = TERA.render("index.html", &ctx).expect(
-        "Failed to render reports",
-    );
-    fs::write(&output_path, &idx).expect("Unable to write file");
 }
--- src/tool_output.rs
+++ src/tool_output.rs
@@ -14,7 +14,7 @@ struct SolcContract {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct MythrilOutput {
-    success: bool,
+    pub success: bool,
     error: Option<String>,
     issues: Vec<MythrilIssue>,
 }
